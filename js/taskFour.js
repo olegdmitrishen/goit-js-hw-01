9f06462833102ff6cf5d95bc0dc653f5e46fcc8f
olegdmitrishen <7mail_lego@ukr.net>
2020-07-14 17:23:39 +0300

    last commit

--- js/taskFour.js
+++ js/taskFour.js
@@ -7,13 +7,12 @@ let message;
 
 if (amountDroids === null) {
 	console.log('Отменено пользователем!');
+} else if (isNaN(amountDroids)) {
+	message = 'Нужно ввести число больше нуля!';
 } else if (amountDroids > 0) {
 	amountDroids = Number(amountDroids);
-
 	const totalPrice = amountDroids * pricePerDroid;
 	const result = credits - totalPrice;
 	message = totalPrice <= credits ? `Вы купили ${amountDroids} дроидов, на счету осталось ${result} кредитов.` : 'Недостаточно средств на счету!';
-} else {
-	message = 'Вы ввели некоректное значение!';
 }
 console.log(message);
